dcd0843058df1896198ebce23abe4484fd2c75e4
acefortin14 <ace.fortin@gsshrsolutions.com>
2026-06-27 12:45:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.ATS_CONFIG = {
-  supabaseUrl: https://gss-ats-static.vercel.app/config.js
-  supabaseAnonKey: "PASTE_YOUR_REAL_SB_PUBLISHABLE_KEY_HERE"
-};
+  supabaseUrl: https://paibkfpagyyaowalyqxa.supabase.co
+  supabaseAnonKey: sb_publishable_PX2gk2DSlQGlloLqfIkdUQ_Bxf5ZioV
