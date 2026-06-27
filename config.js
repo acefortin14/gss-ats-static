71183b15c93305957872cc8d365a580f9716be23
acefortin14 <ace.fortin@gsshrsolutions.com>
2026-06-27 12:11:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.ATS_CONFIG = {
-  supabaseUrl: "https://yourprojectid.supabase.co",
-  supabaseAnonKey: "sb_publishable_your_key_here"
-};
-sb_publishable_PX2gk2DSlQGlloLqfIkdUQ_Bxf5ZioV
+  supabaseUrl: "https://YOUR_PROJECT_REFERENCE.supabase.co",
+  supabaseAnonKey: "sb_publishable_YOUR_PUBLIC_KEY_HERE"
+};sb_publishable_PX2gk2DSlQGlloLqfIkdUQ_Bxf5ZioV
+sb_secret_67uHPC4Dv8TAD3T0gsb2Cg_d550GDKU
